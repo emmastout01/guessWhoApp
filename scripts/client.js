beef32e0dd3d7bcc5ec011aaabbe718e5bb8fa87
Emma Stout <emma.stout01@gmail.com>
2017-10-26 16:30:05 -0500

    commit first files

---
+++ scripts/client.js
@@ -0,0 +1,14 @@
+var guessWhoApp = angular.module('guessWhoApp', []);
+
+console.log('script sourced');
+
+guessWhoApp.controller('guessWhoController', function(){
+    var guessWho = this;
+    guessWho.test = 'test test';
+
+
+
+
+
+
+});
