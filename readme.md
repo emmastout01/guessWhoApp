717180fc0ef31428f61a86e45ca35993f727f568
Emma Stout <emma.stout01@gmail.com>
2017-10-26 17:02:32 -0500

    added input fields and submit button

--- readme.md
+++ readme.md
@@ -1,1 +1,19 @@
-## Test test 
+# Angular Guess Who
+
+## Tasks
+
+Base Mode:
+[x] Create files and source in scripts 
+[x] Create guessWho controller with array of objects in it
+[x] Add 2 input fields in html and bind them to variables in guessWho controller
+[x] Add submit button in HTML
+[ ] Add function in controller for submit button 
+[ ] add li with ng-repeat to loop over each item in the array
+[ ] Set up click function for each image on display so the user name displays at top of page
+
+Hard Mode: 
+[ ]
+[ ]
+[ ]
+[ ]
+[ ]
--- scripts/client.js
+++ scripts/client.js
@@ -4,10 +4,24 @@ console.log('script sourced');
 
 guessWhoApp.controller('guessWhoController', function(){
     var guessWho = this;
-    guessWho.test = 'test test';
+    guessWho.message = "Guess Who!"
+    guessWho.submit = function(person){
+        guessWho.message = "It's " + person + "!"
+        console.log('submit button clicked!')
+    };
 
+    guessWho.persons = [
+        {name: 'Emma', github: 'emmastout01'},
+        {name: 'Chris', github: 'christopherblack'},
+        {name: 'Kris', github: 'kdszafranski'},
+    ]
+    guessWho.name = "";
+    guessWho.github = "";
 
-
+    guessWho.addPerson = function() {
+        var newPerson = {name: guessWho.name, github: guessWho.github};
+        guessWho.persons.push(newPerson)
+    }
 
 
 
