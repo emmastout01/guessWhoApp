32176d2d805b9ff5e7a27947e16687132e8bdc77
Emma Stout <emma.stout01@gmail.com>
2017-10-26 22:04:19 -0500

    base mode complete

--- scripts/client.js
+++ scripts/client.js
@@ -4,27 +4,31 @@ console.log('script sourced');
 
 guessWhoApp.controller('guessWhoController', function(){
     var guessWho = this;
+    guessWho.name = "";
+    guessWho.github = "";
     guessWho.message = "Guess Who!"
-    guessWho.submit = function(person){
-        guessWho.message = "It's " + person + "!"
-        guessWho.addPerson();
-        console.log('submit button clicked!')
-    };
-
+   
     guessWho.persons = [
         {name: 'Emma', github: 'emmastout01'},
         {name: 'Elvis', github: 'elvishang'},
         {name: 'Kris', github: 'kdszafranski'},
     ]
-    guessWho.name = "";
-    guessWho.github = "";
 
     guessWho.addPerson = function() {
         var newPerson = {name: guessWho.name, github: guessWho.github};
         guessWho.persons.push(newPerson)
-        console.log(newPerson);
         console.log('array of people: ', guessWho.persons);
     }
+    
+    guessWho.submit = function(person){
+        guessWho.addPerson();
+    };
+
+    guessWho.imageClicked = function(person) {
+        guessWho.message = "It's " + person + "!"
+    }
+
+   
 
 
 
